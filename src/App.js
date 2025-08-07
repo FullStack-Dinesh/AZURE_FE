5069c81f46f43393c537e6ef3b8623d758fcf750
FullStack-Dinesh <dineshvijay2114@gmail.com>
2025-08-07 14:39:38 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -125,7 +125,7 @@ function App() {
       />
       
       <ChatSection messages={messages} />
-      
+      {answer}
       <QuerySection
         query={query}
         onQueryChange={setQuery}
